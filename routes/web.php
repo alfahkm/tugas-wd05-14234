cf23d211b1c3d6b614984a840b4f673fef4a492e
alfa hkm <alfahkm@example.com>
2025-04-10 02:32:15 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
     return view('layouts.main');
 });
 Route::get('/dokter', function () {
